0fc7f732e1ddbbf80e68276540178301cfde0c48
Daria <paksyalinnd@mail.ru>
2025-09-22 10:01:01 +0300

    Update Dariya.py
    Тестовая модель интерфейса с использованием библиотеки tkinter

--- src/Dariya.py
+++ src/Dariya.py
@@ -1,3 +1,22 @@
+<<<<<<< Updated upstream
 """
 This module for Dariya's work, GUI maker.
 """
+=======
+import tkinter as tk
+
+def click():
+    create.config(text="Сработало")
+
+window = tk.Tk()
+window.title("Главная")
+window.geometry("500x300")
+
+create = tk.Label(window, text="", font=("Arial", 16))
+create.pack(pady=40)
+
+button = tk.Button(window, text="Тыкни", command=click)
+button.pack(pady=10)
+
+window.mainloop()
+>>>>>>> Stashed changes
