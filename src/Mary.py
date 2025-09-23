86454924bd2fa698e2249a4dc049800f43c76166
Maria Gnativ <gnativ.shark@yandex.ru>
2025-09-23 21:18:40 +0300

    Try using module trannsformers
    Bad try

--- src/Mary.py
+++ src/Mary.py
@@ -1,3 +1,22 @@
 """
 This module for Mary's work, LLM engineer.
 """
+import transformers as tr
+import torch
+
+model_name = "openai/gpt-oss-20b"
+tokenizer = tr.AutoTokenizer.from_pretrained(model_name)
+model = tr.AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+pipe = tr.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer)
+
+prompt = "Напиши краткое объяснение, что такое искусственный интеллект."
+
+messages = [{"content": prompt}]
+
+formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+outputs = pipe(formatted_prompt)
+print(outputs[0]['generated_text'])
